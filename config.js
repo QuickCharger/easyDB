41bd2dd386eee223beaf20b3e08c1725f77dd573
bdg <bdg>
2023-07-12 13:37:14 +0800

    init

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const path = require('path')
 const DBName = 'EasyDB'
 module.exports = {
     sequelize: {
-        dialect: 'sqlite',    // 'mysql' || 'sqlite'
-        dbName: DBName,
+        dialect: 'sqlite',  // 'mysql' || 'sqlite'
+        dbName: DBName,     // mysql时的数据库名 或 sqlite时的文件名称
         sqlite3_storage: path.join(__dirname, `${DBName}.db`), // dialect='mysql'失效
         timezone: "+00:00",                                    // dialect='sqlite'失效
         logging: console.log,
